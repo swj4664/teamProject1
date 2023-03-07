74fa107a7cf8104b9ced3870364377204e336e2a
swj4664 <108079743+swj4664@users.noreply.github.com>
2023-03-07 14:24:27 +0900

    230307_v1

---
+++ tlsdnjswjd (2)/js/index.js
@@ -0,0 +1,209 @@
+// --------------- 카카오맵 ---------------
+
+var mapContainer = document.getElementById('map'), // 지도를 표시할 div  
+    mapOption = {
+        center: new kakao.maps.LatLng(36.436373, 128.034173), // 지도의 중심좌표
+        level: 13 // 지도의 확대 레벨
+    };
+
+var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+
+
+// 지도를 전달받은 위도, 경도 값으로 부드럽게 이동시키는 함수
+function panTo(a, b) {
+    var moveLatLon = new kakao.maps.LatLng(a, b);
+    map.panTo(moveLatLon);
+}
+
+
+// 화면에 마커를 표시하는 함수
+var markers = [];
+function addMarker(position) {
+    var marker = new kakao.maps.Marker({
+        position: position
+    });
+    console.log(marker)
+    marker.setMap(map);
+    markers.push(marker);
+}
+
+function setMarkers(map) {
+    for (var i = 0; i < markers.length; i++) {
+        markers[i].setMap(map);
+    }
+}
+
+function showMarkers() {
+    setMarkers(map)
+}
+
+function hideMarkers() {
+    setMarkers(null);
+}
+
+
+// ------------------------------------
+
+
+
+var global = []
+$.get("info.json", function (data) {
+    global = data.records
+    // cityMarker('강원도')     // 초기에 선택된 강원도 마크 표시를 위해 함수 호출
+    useData(data.records)
+});
+
+function cityMarker(city) {   // 시도명을 인수로 받아 마크를 표시하는 함수선언
+    let cityList = global.filter((a) => {
+        let sidoList = a.sidoName.split(' ')
+        return sidoList[0] === city
+    })
+
+    for (let i in cityList) {
+        addMarker(new kakao.maps.LatLng(cityList[i].lat, cityList[i].lng));
+    }
+
+    // 지도위치를 현재 선택한 도시의 첫번째 레코드의 위도, 경도값으로 이동하는 함수 호출
+    panTo(cityList[0].lat, cityList[0].lng)
+    // }
+
+}
+
+// 시도명을 인수로 받아 마크를 표시하는 함수선언
+function cityMarker2(city) {   
+    let cityList = global.filter((a) => {
+        let sidoList = a.sidoName.split(' ')
+        return sidoList[1] === city
+    })
+    
+    for(let i in cityList) {
+        addMarker(new kakao.maps.LatLng(cityList[i].lat, cityList[i].lng));
+    }
+
+    // 지도위치를 현재 선택한 도시의 첫번째 레코드의 위도, 경도값으로 이동하는 함수 호출
+    panTo(cityList[0].lat, cityList[0].lng)
+    
+}
+
+
+
+// ---------------------------------------------------
+
+// 버튼을 클릭하면 아래 배열의 좌표들이 모두 보이게 지도 범위를 재설정합니다 
+
+
+// 지도를 재설정할 범위정보를 가지고 있을 LatLngBounds 객체를 생성합니다
+// var bounds = new kakao.maps.LatLngBounds();    
+
+// var i, marker;
+// for (i = 0; i < points.length; i++) {
+//     // 배열의 좌표들이 잘 보이게 마커를 지도에 추가합니다
+//     marker =     new kakao.maps.Marker({ position : points[i] });
+//     marker.setMap(map);
+    
+//     // LatLngBounds 객체에 좌표를 추가합니다
+//     bounds.extend(points[i]);
+// }
+
+// function setBounds() {
+//     // LatLngBounds 객체에 추가된 좌표들을 기준으로 지도의 범위를 재설정합니다
+//     // 이때 지도의 중심좌표와 레벨이 변경될 수 있습니다
+//     map.setBounds(bounds);
+// }
+
+
+// ---------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+// 시도명을 인수로 받아 구군셀렉트를 나타내는 함수선언
+function sebu(city) {   
+    let sebuList = []
+    let option = ''
+
+    let cityList = global.filter((a) => {
+        let sidoList = a.sidoName.split(' ')
+        if(sidoList[0] === city){
+            return sidoList
+        }
+    })
+    
+    for(let i in cityList){
+        let abc = []
+        abc = cityList[i].sidoName.split(' ')
+        if(abc[1] != null){
+            sebuList.push(abc[1])
+        }
+    }
+    
+    let sebuIndex = sebuList.filter((a, b) => sebuList.indexOf(a) === b);
+    sebuIndex.sort()
+
+    sebuIndex.map((a)=> {
+        option = `<option>전체</option>`
+    })
+
+    sebuIndex.map((a)=> {
+        option += `<option value="${a}">${a}</option>`
+    })
+
+    map.setLevel(12)
+   
+    panTo(cityList[0].lat, cityList[0].lng)
+    $('#detail').html(option)
+    $('#detail').html(option).change(()=>{
+        map.setLevel(11)
+        panTo(cityList[0].lat, cityList[0].lng)
+    })
+    
+
+}
+
+
+// 시도 목록을 button 상자에 표시하는 함수
+function useData(globalData) {
+    let list = globalData.map((a, b) => {
+        let sidoList = a.sidoName.split(' ')
+        return sidoList[0]
+    })
+
+    let cityIndex = list.filter((a, b) => list.indexOf(a) === b);
+    cityIndex.sort()
+ 
+    let button = ''
+
+    cityIndex.map((value) => {
+        button += `<button id="btnn" type="button" value="${value}">${value}</button>`
+    })
+
+    $('#btn').html(button)
+}
+
+
+//이벤트
+$('#detail').change(function () {
+    hideMarkers()   // 선택을 변경했을때 표시된 마커 숨기기
+    let city = $(this).val()    // 선택된 시도명을 추출해서
+    cityMarker2(city)   // 마커를 표시하는 함수를 호출문에 넣어줌
+})
+
+$('#btn').click((e) => {
+    hideMarkers()
+    let city = e.target.value
+    cityMarker(city)
+    sebu(city)
+    $(this).addClass('on')
+})
+
+
+
+
